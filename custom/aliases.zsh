5d32651c26c3c263a6497d134f953ca578bdd9c4
csm <sergei.churbanov@gmail.com>
2022-05-21 13:44:08 +0300

    update l alias to use directory grouping

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -6,3 +6,8 @@ gex () {#run smartGit as git extensions
     setopt local_options no_notify no_monitor
     nohup smartgit . $@ &> /dev/null &disown
 }
+
+#to find all aliases stratr with:
+#grep -rl alias ~/.bash* ~/.profile /etc/profile /etc/bash.bashrc ~/.oh-my-zsh
+
+alias l='ls -lah --group-directories-first'
